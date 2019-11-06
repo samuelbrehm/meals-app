5e7c266479073f309ca37bd7270fa17882433217
Samuel Brehm <samuelbrehm@gmail.com>
2019-11-06 09:16:20 -0300

    Created category meals component

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,24 +1,56 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridTile from "../components/CategoryGridTile";
 
 export default function CategoriesScreen(props) {
-  return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button
-        title="Go to Category Meals!"
-        onPress={() => {
-          props.navigation.navigate({ routeName: "CategoryMeals" });
+  function renderGridItem(itemData) {
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onSelect={() => {
+          props.navigation.navigate({
+            routeName: "CategoryMeals",
+            params: {
+              categoryId: itemData.item.id
+            }
+          });
         }}
       />
-    </View>
+    );
+  }
+
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
   );
 }
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: "Meal Categories"
+};
 
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150
   }
 });
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,16 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Platform } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
 
 export default function CategoryMealScreen(props) {
+  const catId = props.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="Go to Meals Details"
         onPress={() => {
@@ -14,6 +20,19 @@ export default function CategoryMealScreen(props) {
     </View>
   );
 }
+
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? selectedCategory.color : ""
+    },
+    headerTintColor: Platform.OS === "android" ? "#fff" : selectedCategory.color
+  };
+};
 
 const styles = StyleSheet.create({
   screen: {
