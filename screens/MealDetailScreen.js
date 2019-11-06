198b19afa83b35d0adc2a977251d261e75ad77d4
Samuel Brehm <samuelbrehm@gmail.com>
2019-11-06 14:58:40 -0300

    Created screen meal details with add favorite custom header button

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { MEALS } from "../data/dummy-data";
+import CustomHeaderButton from "../components/HeaderButton";
 
 export default function MealDetailScreen(props) {
   const mealId = props.navigation.getParam("mealId");
@@ -19,7 +21,18 @@ MealDetailScreen.navigationOptions = navigationData => {
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Favorite"
+          iconName="ios-star"
+          onPress={() => {
+            console.log("Mark as favorite!");
+          }}
+        />
+      </HeaderButtons>
+    )
   };
 };
 
