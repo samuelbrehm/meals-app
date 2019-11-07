1302d7e92dc6beec83560353167aadd44be0b691
Samuel Brehm <samuelbrehm@gmail.com>
2019-11-07 11:14:09 -0300

    Updated meal details screen with styles

---
+++ components/ListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+import DefaultText from "./DefaultText";
+
+export default function ListItem(props) {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  listItem: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 10
+  }
+});
